5f917a37f1a36868ba72eb183a5e1805307b933e
razak <ipoxe@mail.ru>
2022-06-20 18:27:09 +0300

    add less2

---
+++ less2/scr/main.js
@@ -0,0 +1,10 @@
+import { formHandle, formResetHandler } from "./formHandlers.js";
+import { menuSetup } from "./menu.js";
+import "../sound/sound.mp3";
+
+const dateCalcForm = document.getElementById("datecalc");
+const timerForm = document.getElementById("timer");
+dateCalcForm.addEventListener("submit", formHandle);
+timerForm.addEventListener("submit", formHandle);
+timerForm.addEventListener("reset", formResetHandler);
+menuSetup(document.querySelector(".menu"));
